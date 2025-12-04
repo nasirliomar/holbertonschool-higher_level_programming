2f013911899697b9919463984e1a19f70726dacf
nasirliomar <nesirliomer019@gmail.com>
2025-12-04 13:02:28 +0000

    l

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -3,8 +3,7 @@ if __name__ == "__main__":
     import sys
 
     total = 0
-    # Loop through arguments starting from index 1 (skipping the script name)
     for i in range(1, len(sys.argv)):
         total += int(sys.argv[i])
-        
+
     print(total)
