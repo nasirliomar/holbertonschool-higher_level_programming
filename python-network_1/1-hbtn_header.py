94cfaa7d873f82e973a635d0e070833a4495327a
nasirliomar <nesirliomer019@gmail.com>
2025-12-14 15:14:20 +0000

    l

---
+++ python-network_1/1-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Displays the X-Request-Id header from a URL response"""
+
+import sys
+from urllib import request
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    with request.urlopen(url) as response:
+        print(response.headers.get("X-Request-Id"))
