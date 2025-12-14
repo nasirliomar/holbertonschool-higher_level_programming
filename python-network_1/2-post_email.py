b3594704b212b9a40f04928dead8ed722edb59d2
nasirliomar <nesirliomer019@gmail.com>
2025-12-14 15:15:29 +0000

    l

---
+++ python-network_1/2-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Sends a POST request with an email parameter using urllib"""
+
+import sys
+from urllib import request, parse
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    data = parse.urlencode({'email': email}).encode('utf-8')
+
+    with request.urlopen(url, data) as response:
+        print(response.read().decode('utf-8'))
